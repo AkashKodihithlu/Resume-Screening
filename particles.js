34ff7e6b781c221ac89cb1c8e6a8f8ea78c1e1bf
Akash <akash.kodihithlu@gmail.com>
2026-01-17 23:13:15 +0530

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,29 @@
+particlesJS("particles-js",{
+    particles:{
+        number:{
+            value:90,density:{ enable:true,value_area:900}
+        },
+        color:{value:"#ffffff"},
+        shape:{ type:"circle"},
+        opacity:{value:0.5},
+        size:{value:2},
+        line_linked:{
+            enable:true,
+            distance:150,
+            color:"#ffffff",
+            opacity:0.3,
+            width:1
+        },
+        move:{
+            enable:true,
+            speed:1,
+            out_mode:"out"
+        }
+    },
+    interactivity:{
+        events:{
+            onhover: {enable: true,mode:"grab"}
+        }
+    },
+    retina_detect:true
+});ff
--- script.js
+++ script.js
@@ -6,4 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.signup')?.addEventListener('click', () => {
         window.location.href = 'signup.html';
     });
+
+    document.querySelector('.signin-nav')?.addEventListener('click',()=> {
+        window.location.href = 'index.html';
+    
+    });
 });
